78b49c1be5081dc8cdd005f5524e4c1eb2d19e25
Lin Onetwo <linonetwo012@gmail.com>
2022-02-07 00:00:31 +0800

    feat: argument tiddlywiki

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,6 @@
-import { I$TW } from './tw';
-export * from './tw';
-export * from './filter-operator';
+import './tw';
+import './filter-operator';
 
-declare module 'tiddlywiki' {}
-
-declare module '@tiddlygit/tiddlywiki' {}
-
-declare global {
-  var $tw: I$TW;
+declare module '@tiddlygit/tiddlywiki' {
+  export * from 'tiddlywiki';
 }
