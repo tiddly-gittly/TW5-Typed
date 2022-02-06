ca64620768c6f92cb4ed1708e110ca0cc8d0d1fe
Lin Onetwo <linonetwo012@gmail.com>
2022-02-07 00:00:31 +0800

    feat: argument tiddlywiki

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Add `tw5-typed` to your `tsconfig.json`'s `compilerOptions`
 ```json
 {
   "compilerOptions": {
-    "types": ["node", "tw5-typed"] /* Type declaration files to be included in compilation. */,
+    "types": ["node", "tw5-typed"] /* Type declaration files to be included in compilation. */
   }
 }
 ```
@@ -23,5 +23,15 @@ Add `tw5-typed` to your `tsconfig.json`'s `compilerOptions`
 Then you will have global types like `$tw` automatically. You can import the rest of the types using `import type` statement:
 
 ```typescript
-
+import type { ISearchOptions, SourceIterator, IFilterOperatorParamOperator } from 'tw5-typed';
 ```
+
+## Development
+
+You can add new `*.d.ts` file to contain your types:
+
+1. use `declare module 'tiddlywiki' { }` to wrap all your types.
+1. don't forget to `export` all your types.
+1. to add type for global variable, add `global { }` inside that `declare module 'tiddlywiki' { }`, like `global { var $tw: I$TW; }`
+
+And add `import './xxx';` in the `index.d.ts` file.
--- src/tw.d.ts
+++ src/tw.d.ts
@@ -1,49 +1,55 @@
-export interface ITiddler {
-  cache: ITiddlerCache;
-  fields: ITiddlerFields;
-}
+declare module 'tiddlywiki' {
+  export interface ITiddler {
+    cache: ITiddlerCache;
+    fields: ITiddlerFields;
+  }
 
-export interface ITiddlerFields {
-  created: string;
-  list: string[];
-  modified: string;
-  tags: string[];
-  text: string;
-  title: string;
-  type: string;
-  [anyKey: string]: unknown;
-}
+  export interface ITiddlerFields {
+    created: string;
+    list: string[];
+    modified: string;
+    tags: string[];
+    text: string;
+    title: string;
+    type: string;
+    [anyKey: string]: unknown;
+  }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ITiddlerCache {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface ITiddlerCache {}
 
-/**
- * filepath: '/Users/linonetwo/xxxx/wiki/Meme-of-LinOnetwo/tiddlers/tiddlerTitle.tid',
- * hasMetaFile: false
- * tiddlerTitle: string,
- * type: 'application/x-tiddler',
- */
-export interface IBootFilesIndexItem {
-  filepath: string;
-  hasMetaFile: boolean;
-  tiddlerTitle: string;
-  type: string;
-}
-/**
- * Record<tiddlerTitle, IBootFilesIndexItem>
- */
-export type IBootFilesIndex = Partial<Record<string, IBootFilesIndexItem>>;
+  /**
+   * filepath: '/Users/linonetwo/xxxx/wiki/Meme-of-LinOnetwo/tiddlers/tiddlerTitle.tid',
+   * hasMetaFile: false
+   * tiddlerTitle: string,
+   * type: 'application/x-tiddler',
+   */
+  export interface IBootFilesIndexItem {
+    filepath: string;
+    hasMetaFile: boolean;
+    tiddlerTitle: string;
+    type: string;
+  }
+  /**
+   * Record<tiddlerTitle, IBootFilesIndexItem>
+   */
+  export type IBootFilesIndex = Partial<Record<string, IBootFilesIndexItem>>;
 
-export interface I$TW {
-  boot: { argv: string[]; files: IBootFilesIndex; startup: (options: { callback?: () => unknown }) => void };
-  hooks: { addHook: (hookName: string, callback: (...arguments_: any[]) => unknown) => void };
-  wiki: {
-    getTiddler: (title: string) => ITiddler | undefined;
-    /**
-     * Get full list of tiddler titles in the wiki
-     */
-    getTiddlers: () => string[];
-  };
-  utils: Record<string, any>;
+  export interface I$TW {
+    boot: { argv: string[]; files: IBootFilesIndex; startup: (options: { callback?: () => unknown }) => void };
+    hooks: { addHook: (hookName: string, callback: (...arguments_: any[]) => unknown) => void };
+    wiki: {
+      getTiddler: (title: string) => ITiddler | undefined;
+      /**
+       * Get full list of tiddler titles in the wiki
+       */
+      getTiddlers: () => string[];
+    };
+    utils: Record<string, any>;
+  }
+  export function TiddlyWiki(): I$TW;
+
+  global {
+    var $tw: I$TW;
+  }
 }
-export function TiddlyWiki(): I$TW;
