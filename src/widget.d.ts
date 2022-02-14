c73fcc16c3e3f9630e3397714b2d80020b190779
Lin Onetwo <linonetwo012@gmail.com>
2022-02-15 00:09:21 +0800

    fix: export ts type from sub files

--- src/widget.d.ts
+++ src/widget.d.ts
@@ -1,26 +1,37 @@
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 declare class variablesConstructor {}
 
+export declare class Widget {
+  constructor(parseTreeNode: unknown, options: unknown);
+  initialize: (parseTreeNode: unknown, options: unknown) => void;
+  parseTreeNode: unknown;
+  wiki: unknown;
+  parentWidget?: Widget;
+  variablesConstructor: variablesConstructor;
+  variables: unknown;
+  domNodes: Node[];
+  /**
+    Add an event listener
+  */
+  addEventListener(type: string, handler: (args: any[]) => void): void;
+  /**
+    Dispatch an event to a widget. If the widget doesn't handle the event then it is also dispatched to the parent widget
+  */
+  dispatchEvent(type: string): void;
+  /**
+    Add a list of event listeners from an array [{type:,handler:},...]
+  */
+  addEventListeners(listeners: { type: string; handler: (args: any[]) => void }[]): void;
+
+  parentDomNode: Node;
+  execute: () => void;
+
+  /**
+   * Lifecycle method: Render this widget into the DOM
+   */
+  render(parent: Node, nextSibling: Node): void;
+  computeAttributes(): void;
+}
 declare module 'tiddlywiki' {
-  export class Widget {
-    constructor(parseTreeNode: unknown, options: unknown);
-    initialize: (parseTreeNode: unknown, options: unknown) => void;
-    parseTreeNode: unknown;
-    wiki: unknown;
-    parentWidget?: Widget;
-    variablesConstructor: variablesConstructor;
-    variables: unknown;
-    /**
-      Add an event listener
-    */
-    addEventListener: (type: string, handler: (args: any[]) => void) => void;
-    /**
-      Dispatch an event to a widget. If the widget doesn't handle the event then it is also dispatched to the parent widget
-    */
-    dispatchEvent: (type: string) => void;
-    /**
-      Add a list of event listeners from an array [{type:,handler:},...]
-    */
-    addEventListeners: (listeners: { type: string; handler: (args: any[]) => void }[]) => void;
-  }
+  export type Widget = typeof Widget;
 }
