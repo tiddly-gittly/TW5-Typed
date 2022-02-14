bfede65806bdaac70580ab581ae3f255b246d73d
Lin Onetwo <linonetwo012@gmail.com>
2022-02-15 00:09:47 +0800

    test: widget

---
+++ src/tiddlywiki-test.ts
@@ -0,0 +1,17 @@
+import { Widget as IWidget } from 'tiddlywiki'
+
+const Widget = {} as unknown as IWidget;
+export class ReactWidget extends Widget {
+  constructor(parseTreeNode: any, options: any) {
+    super(parseTreeNode, options);
+  }
+
+  render(parent: Node, nextSibling: Node) {
+    this.parentDomNode = parent;
+    this.execute();
+    this.computeAttributes();
+    const containerElement = document.createElement('div');
+    this.domNodes.push(containerElement);
+    parent.appendChild(containerElement);
+  }
+}
