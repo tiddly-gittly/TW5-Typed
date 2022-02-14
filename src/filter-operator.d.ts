70b737aff1b2e271404bfd9c547288f64613d70c
Lin Onetwo <linonetwo012@gmail.com>
2022-02-14 21:09:21 +0800

    refactor: filter to be more readable

--- src/filter-operator.d.ts
+++ src/filter-operator.d.ts
@@ -1,44 +1,4 @@
 declare module 'tiddlywiki' {
-  export type SourceIterator = (tiddler: Tiddler, title: string) => void;
-  export interface ISearchOptions {
-    /** If true, forces all but regexp searches to be anchored to the start of text */
-    anchored?: boolean;
-    /** If true forces a case sensitive search */
-    caseSensitive?: boolean;
-    /** An array of tiddler titles to exclude from the search */
-    exclude?: string[];
-    /** If true, the field options are inverted to specify the fields that are not to be searched */
-    excludeField?: boolean;
-    /** If specified, restricts the search to the specified field, or an array of field names */
-    field?: string | string[];
-    /** If true returns tiddlers that do not contain the specified string */
-    invert?: boolean;
-    /** searches for literal string */
-    literal?: boolean;
-    /** an iterator function for the source tiddlers, called source(iterator), where iterator is called as iterator(tiddler,title) */
-    source?: (iter: SourceIterator) => void;
-    /** same as literal except runs of whitespace are treated as a single space */
-    whitespace?: boolean;
-    /** (default) treats search string as a list of tokens, and matches if all tokens are found, regardless of adjacency or ordering */
-    words?: boolean;
-  }
-
-  export interface IFilterOperatorParameterOperator {
-    /** the operand for the filter step (as a string; if the filter specified it in angle brackets or braces, the text reference or letiable name will have already been resolved); */
-    operand: string;
-    /** the name of the filter operator specified in the WikiText; */
-    operator: string;
-    /** (optional) a string containing a single exclamation mark if the filter operator is to be negated; */
-    prefix?: string;
-    /** (optional, deprecated) used instead of `operand` if the filter operand is a regexp. */
-    regexp?: string;
-    /** (optional) a string containing an additional filter argument (typically a tiddler field name) following the filter name (separated by a colon in the filter syntax); */
-    suffix?: string;
-    /** multiple suffix
-     * for example, in `search:<field list>:<flag list>[<operand>]`, you will get `<field list>` as suffixes[0], and `<flag list>` as suffixes[1]
-     */
-    suffixes?: string[][];
-  }
   /**
    * # Overview
 
@@ -134,4 +94,59 @@ As with [JavaScript Macros](#JavaScript%20Macros), filter operators should not m
 
    */
   export type IFilterOperator = (source: (iter: SourceIterator) => void, operator: IFilterOperatorParamOperator) => string[] | ((iter: SourceIterator) => void);
+
+  /**
+   * A [tiddler iterator](#Tiddler%20Iterators) representing the results of the previous filter step (or all tiddlers, if this filter appears first in an expression), conventionally named `source`.
+   * 
+   * For Example, with an iterator over all the tiddlers tagged as `interesting`, use it like this:
+   * 
+   * ```js
+   * var result = [];
+      var include = true;
+      source(function(tiddler, title) {
+          if (include) { result.push(title); }
+          include = !include;
+      });
+    ```
+   */
+  export type SourceIterator = (tiddler: Tiddler, title: string) => void;
+  export interface ISearchOptions {
+    /** If true, forces all but regexp searches to be anchored to the start of text */
+    anchored?: boolean;
+    /** If true forces a case sensitive search */
+    caseSensitive?: boolean;
+    /** An array of tiddler titles to exclude from the search */
+    exclude?: string[];
+    /** If true, the field options are inverted to specify the fields that are not to be searched */
+    excludeField?: boolean;
+    /** If specified, restricts the search to the specified field, or an array of field names */
+    field?: string | string[];
+    /** If true returns tiddlers that do not contain the specified string */
+    invert?: boolean;
+    /** searches for literal string */
+    literal?: boolean;
+    /** an iterator function for the source tiddlers, called source(iterator), where iterator is called as iterator(tiddler,title) */
+    source?: (iter: SourceIterator) => void;
+    /** same as literal except runs of whitespace are treated as a single space */
+    whitespace?: boolean;
+    /** (default) treats search string as a list of tokens, and matches if all tokens are found, regardless of adjacency or ordering */
+    words?: boolean;
+  }
+
+  export interface IFilterOperatorParameterOperator {
+    /** the operand for the filter step (as a string; if the filter specified it in angle brackets or braces, the text reference or letiable name will have already been resolved); */
+    operand: string;
+    /** the name of the filter operator specified in the WikiText; */
+    operator: string;
+    /** (optional) a string containing a single exclamation mark if the filter operator is to be negated; */
+    prefix?: string;
+    /** (optional, deprecated) used instead of `operand` if the filter operand is a regexp. */
+    regexp?: string;
+    /** (optional) a string containing an additional filter argument (typically a tiddler field name) following the filter name (separated by a colon in the filter syntax); */
+    suffix?: string;
+    /** multiple suffix
+     * for example, in `search:<field list>:<flag list>[<operand>]`, you will get `<field list>` as suffixes[0], and `<flag list>` as suffixes[1]
+     */
+    suffixes?: string[][];
+  }
 }
