78806b9dddf186f575995e92b8937c3a418e9dbf
Lin Onetwo <linonetwo012@gmail.com>
2022-08-23 13:55:09 +0800

    Update ast.d.ts

--- src/ast.d.ts
+++ src/ast.d.ts
@@ -12,6 +12,7 @@ declare module 'tiddlywiki' {
     children?: IParseTreeNode[];
     end?: number;
     isBlock?: boolean;
+    isMacroDefinition?: boolean;
     isSelfClosing?: boolean;
     orderedAttributes?: IParseTreeAttribute[];
     start?: number;
